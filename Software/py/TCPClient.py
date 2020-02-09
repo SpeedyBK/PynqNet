ac145a7b58d41827e897ae8b7780edd5d2299ea4
Benjamin Lagershausen-Kessler <benjamin.kessler01@googlemail.com>
2020-02-09 20:44:38 +0100

    Organized Python Code

--- Software/py/TCPClient.py
+++ Software/py/TCPClient.py
@@ -3,44 +3,74 @@ import sys
 import argparse
 import socket
 
-host = "192.168.2.101"
-port = 8000
+#####################
+# Parse Commandline #
+#####################
+def parse_cmd():
+    parser = argparse.ArgumentParser(description="UDP Sender Script")
+    parser.add_argument("-f", "--file", dest="filename", default="", help="read data from FILE",
+                        metavar="FILE")
+    parser.add_argument("-s", "--server", dest="server", default="",
+                        help="destination host HOST", metavar="HOST")
+    parser.add_argument("-p", "--port", dest="port", default="", help="destination port PORT",
+                        metavar="PORT")
+    parser.add_argument('cmd', nargs=argparse.REMAINDER)
+    return parser.parse_args()
 
-parser = argparse.ArgumentParser(description="UDP Sender Script")
-parser.add_argument("-f", "--file", dest="filename", default="", help="read data from FILE",
-                    metavar="FILE")
-parser.add_argument("-s", "--server", dest="server", default="",
-                    help="destination host HOST", metavar="HOST")
-parser.add_argument("-p", "--port", dest="port", default="", help="destination port PORT",
-                    metavar="PORT")
-parser.add_argument('cmd', nargs=argparse.REMAINDER)
-args = parser.parse_args()
 
-if args.server == "":
-    host = "192.162.2.101"
-else:
-    host = args.server
+###################################
+# Generate Message from Arguments #
+###################################
+def get_message_data(args):
+    # Check Arguments vs Defaults #
+    if args.server != "":
+        host = args.server
+    else:
+        host = "192.168.2.101"
 
-if args.port == "":
-    port = 8000
-else:
-    port = int(args.port)
+    if args.port != "":
+        port = int(args.port)
+    else:
+        port = 8000
 
-if args.filename == "":
-    data = ' '.join(args.cmd)
-elif args.filename == "stdin":
-    data = sys.stdin.readlines()
-else:
-    filename = os.path.abspath(args.filename)
-    if not os.path.exists(filename):
-        print >> sys.stderr, "The file %s does not exist!" % filename
-        os._exit(1)
-    with open(filename) as fh:
-        data = fh.read()
+    # Check Message if file provided, read file #
+    if args.filename == "":
+        data = ' '.join(args.cmd)
+    elif args.filename == "stdin":
+        data = sys.stdin.readlines()
+    else:
+        filename = os.path.abspath(args.filename)
+        if not os.path.exists(filename):
+            print(sys.stderr, "The file %s does not exist!" % filename)
+            os._exit(1)
+        with open(filename) as fh:
+            data = fh.read()
 
-addr = (host, port)
+    _message = (host, port, data)
+    return _message
+
+
+################
+# Main Program #
+################
+
+# Parse Commandeline #
+arguments = parse_cmd()
+
+# Generate Message # - ([0] Remote IP, [1] Remote Port, [2] Data to send) #
+message = get_message_data(arguments)
+
+# TCP Connection #
+addr = (message[0], message[1])
+
+# Socket Setup #
 TCPSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 TCPSock.connect(addr)
-TCPSock.send(data.encode())
+
+# Send Message #
+TCPSock.send(message[2].encode())
+
+# Close Socket #
 TCPSock.close()
+
 exit(0)
